4a5ec2ffa423e440a98f39f0d85bcf4ad95fabeb
Naveen <thenvnsahu@gmail.com>
2020-03-01 01:21:39 +0530

    organized and split style files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
-const gulpIf = require("gulp-if");
 const cleanCSS = require("gulp-clean-css");
 const babel = require("gulp-babel");
 const autoprefixer = require("gulp-autoprefixer");
