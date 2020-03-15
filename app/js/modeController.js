72c9e004ebd37a4dbcf2c0d4d75d2aad3f3841f2
Naveen <thenvnsahu@gmail.com>
2020-03-16 01:44:55 +0530

    styled dark mode

--- app/js/modeController.js
+++ app/js/modeController.js
@@ -3,9 +3,11 @@ modeCheckbox.addEventListener("click", e => {
     console.log("Day Mode");
     sliderIcon.classList.remove("fa-moon");
     sliderIcon.classList.add("fa-sun");
+    document.body.classList.remove("dark");
   } else {
     console.log("Night Mode");
     sliderIcon.classList.remove("fa-sun");
     sliderIcon.classList.add("fa-moon");
+    document.body.classList.add("dark");
   }
 });
